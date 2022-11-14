51baca70e17b1a37342ffd83878272b93927dd66
AleksandrKonst <genkonst5@gmail.com>
2022-11-14 10:24:45 +0300

    add page links

--- VideoCardsStore/Controllers/HomeController.cs
+++ VideoCardsStore/Controllers/HomeController.cs
@@ -1,20 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VideoCardsStore.Models;
+using VideoCardsStore.Models.ViewModels;
 
 namespace VideoCardsStore.Controllers
 {
     public class HomeController : Controller
     {
         private IStoreRepository repository;
+        public int PageSize = 4;
 
         public HomeController(IStoreRepository repo)
         {
             repository = repo;
         }
 
-        public IActionResult Index()
-        {
-            return View(repository.Products);
-        }
+        public ViewResult Index(int productPage = 1)
+         => View(new ProductsListViewModel
+         {
+             Products = repository.Products
+                 .OrderBy(p => p.ProductId)
+                 .Skip((productPage - 1) * PageSize)
+                 .Take(PageSize),
+             PagingInfo = new PagingInfo
+             {
+                 CurrentPage = productPage,
+                 ItemsPerPage = PageSize,
+                 TotalItems = repository.Products.Count()
+             }
+         });
     }
 }
